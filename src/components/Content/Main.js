320000699f9a288500ccbc6fd875af4d6e3834dc
Joffreynk <joyjoffrey@gmail.com>
2023-02-05 23:10:19 +0200

    move carousel collections in its own component

--- src/components/Content/Main.js
+++ src/components/Content/Main.js
@@ -1,36 +1,16 @@
-import React, {useState} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import Collections from './Collections'
 
-import {collections} from '../services/index'
+
 
 const Main = props => {
-  const [position, setPosition] = useState(0)
-  const next = () => {
-    if (position === collections.length-1) {
-      setPosition(0)
-    }else {
-      setPosition(position+1);
-    }
-  }
 
-  const Prev = () => {
-    if (position === 0) {
-      setPosition(collections.length-1)
-    }else {
-      setPosition(position-1);
-    }
-  }
 
   return (
   <main className='mt-20'>
-    <section id="collections" className="relative">
-      <img className="w-full h-64" src={collections[position].pic} alt="Collections"/>
-      <div className='absolute top-[50%] right-0 left-0 flex justify-between items-end text-black text-xlg px-5'>
-        <button className=" -rotate-90 uppercase  text-3xl" onClick={Prev} >Prev</button>
-        <p>{collections[position].message}</p>
-        <button className="-rotate-90 uppercase text-3xl" onClick={next} >Next</button>
-      </div>
-    </section>
+    {/* move carousel collections in its own component */}
+    <Collections />
   </main>
   )
 }
