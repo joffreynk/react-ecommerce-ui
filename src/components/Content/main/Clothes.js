d0356dfdd59dd9a564374ce846ace17c90a06107
Joffreynk <joyjoffrey@gmail.com>
2023-02-06 08:56:44 +0200

    create clothes component & modify data

--- src/components/Content/main/Clothes.js
+++ src/components/Content/main/Clothes.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Cloth from './Cloth'
 
 const Clothes = () => {
   return (
-    <div>Clothes</div>
+    <div className='mx-24'>
+      <div className='flex gap-4 mx-[5%] text-right'>
+        <h3 className='text-3xl font-semibold uppercase text-black'>Men & women</h3>
+        <ul className='flex gap-5 justify-center items-center pt-3'>
+          <li>Best deals</li>
+          <li>Featured</li>
+          <li>Best seller</li>
+          <li>New Arrivals</li>
+          <li>Sale</li>
+          <li>Accessories</li>
+        </ul>
+      </div>
+      <Cloth />
+      <Cloth />
+      <Cloth />
+      <Cloth />
+      <Cloth />
+      <Cloth />
+    </div>
   )
 }
 
--- src/components/services/index.js
+++ src/components/services/index.js
@@ -2,8 +2,19 @@ import women4 from '../../images/women4.jpeg';
 import women3 from '../../images/women3.jpeg';
 import poster from '../../images/poster.jpg';
 
+import women1 from '../../images/women1.jpeg';
+import women2 from '../../images/women2.jpeg';
+import accessory1 from '../../images/accessory1.jpg';
+import accessory2 from '../../images/accessory2.jpg';
+import accessory3 from '../../images/accessory3.jpeg';
+import accessory4 from '../../images/accessory4.jpeg';
+
 export const collections = [
   {pic: women3, message:'Happy woman  form unknown country', title:'Women Eye Glasses'},
   {pic: women4, message:'White color with white clothes', title:'Women Jackets ever'},
   {pic: poster, message:'Selling product accross all countries', title:'Women collections'}
+]
+
+export const clothes = [
+  {pic: women3, },
 ]
