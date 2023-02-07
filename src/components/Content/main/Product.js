64f6d195e7287eb14fef2747124bb068e65906db
Joffreynk <joyjoffrey@gmail.com>
2023-02-08 00:07:25 +0200

    add compoents to eeach product to track the shopping cart

--- src/components/Content/main/Product.js
+++ src/components/Content/main/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {BsStar} from 'react-icons/bs'
+import {BsStar, BsCart4, BsSuitHeart} from 'react-icons/bs'
+import {MdOutlineCropRotate} from 'react-icons/md'
 
 const Product = ({product}) => {
   const numberRating = [1, 2, 3, 4, 5]
@@ -11,8 +12,17 @@ const Product = ({product}) => {
       <p className='flex'>
         {numberRating.map(number=><BsStar />)}
       </p>
-      <p>{product.price}</p>
+      <div className='flex gap-10'>
+        <p>${product.price}</p>
+        <del>${product.price+Number(10*Math.random().toFixed(3))}</del>
       </div>
+      <div className='flex'>
+        <BsCart4 />
+        <BsSuitHeart />
+        <MdOutlineCropRotate />
+      </div>
+
+    </div>
   )
 }
 
