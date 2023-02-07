0e3b239c0d57a4d9e120d8400ab15cc5f7c6dd89
Joffreynk <joyjoffrey@gmail.com>
2023-02-07 22:54:51 +0200

    style main part of clothes

--- src/components/Content/main/Clothes.js
+++ src/components/Content/main/Clothes.js
@@ -3,16 +3,16 @@ import Cloth from './Cloth'
 
 const Clothes = () => {
   return (
-    <div className='mx-24'>
-      <div className='flex gap-4 mx-[5%] text-right'>
+    <div className='mx-10'>
+      <div className='flex gap-4 mx-[5%] justify-end'>
         <h3 className='text-3xl font-semibold uppercase text-black'>Men & women</h3>
-        <ul className='flex gap-5 justify-center items-center pt-3'>
-          <li>Best deals</li>
-          <li>Featured</li>
-          <li>Best seller</li>
-          <li>New Arrivals</li>
-          <li>Sale</li>
-          <li>Accessories</li>
+        <ul className='flex gap-5 justify-center items-center pt-3 group-hover:cursor-pointer'>
+          <li className='hover:cursor-pointer'>Best deals</li>
+          <li className='hover:cursor-pointer'>Featured</li>
+          <li className='hover:cursor-pointer'>Best seller</li>
+          <li className='hover:cursor-pointer'>New Arrivals</li>
+          <li className='hover:cursor-pointer'>Sale</li>
+          <li className='hover:cursor-pointer'>Accessories</li>
         </ul>
       </div>
       <Cloth />
