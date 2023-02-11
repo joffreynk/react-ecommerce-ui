3badc67b00541c07073aeeeadda18265a098c916
Joffreynk <joyjoffrey@gmail.com>
2023-02-11 10:55:10 +0200

    create initial states for a users

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
-import { Route, Router, Routes } from 'react-router-dom'
+import React, {useReducer, createContext} from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 import Root from './Content/index'
 import CreateProduct from './Dashboard/createProduct'
 import ReadProducts from './Dashboard/products/ReadProducts'
 import Update from './Dashboard/products/update'
+const initialState = {
+  products: [],
+  orders: [],
+  reviews: [],
+  token: {}
+}
+
+const adminInitialState = {
+  ...initialState,
+  users: []
+}
 
 
 
