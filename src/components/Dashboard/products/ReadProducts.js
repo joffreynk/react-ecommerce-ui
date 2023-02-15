b670359cf240a7960d7a8d4c3ecb01a9a582c9fd
Joffreynk <joyjoffrey@gmail.com>
2023-02-15 21:02:43 +0200

    add table template

--- src/components/Dashboard/products/ReadProducts.js
+++ src/components/Dashboard/products/ReadProducts.js
@@ -3,7 +3,23 @@ import { GETALL } from '../../services/actions'
 
 const ReadProducts = ({setFetchHelper}) => {
   return (
-    <div>ReadProducts
+    <div>
+      <table class="table-fixed">
+        <thead>
+          <tr>
+            <th>Song</th>
+            <th>Artist</th>
+            <th>Year</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+            <td>Malcolm Lockyer</td>
+            <td>1961</td>
+          </tr>
+        </tbody>
+      </table>
       <button onClick={()=>setFetchHelper({url: 'productts', type: GETALL, dataUpdate:'products', headers:{}})}>new fetch</button>
     </div>
   )
